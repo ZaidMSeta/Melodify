23925d2de5adaf599f0317b6b19085961b6804a8
Zaid Seta <zaidmseta@gmail.com>
2023-11-02 09:52:58 -0400

    Added search bar functionality

--- searchbar.js
+++ searchbar.js
@@ -1,11 +1,21 @@
 const songCardTemplate = document.querySelector("[data-song-template]")
 const songCardContainer = document.querySelector("[data-song-cards-container]")
+const searchInput = document.querySelector("[data-search]")
 
+let songs = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    songs.forEach(song => {
+        const isVisible = song.songname.includes(value) || song.artist.includes(value)
+        song.element.classList.toggle('hide', !isVisible)
+    })
+})
 
 fetch('songdata.json')
 .then(res => res.json())
 .then(data => {
-    data.forEach(song => {
+    songs = data.map(song => {
         const card = songCardTemplate.content.cloneNode(true).children[0]
         const image = card.querySelector("data-img")
         const header = card.querySelector("[data-header]")
@@ -13,5 +23,6 @@ fetch('songdata.json')
         header.textContent = song.songname
         body.textContent = song.artist
         songCardContainer.append(card)
+        return { name: song.songname, artist: song.artist, element: card }
     })
 })
