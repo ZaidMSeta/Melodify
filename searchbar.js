9468dd627df6a28a1e1f3ee1267e54c62ebc5501
Zaid Seta <144247476+ZaidMSeta@users.noreply.github.com>
2023-11-05 14:35:12 -0500

    Update searchbar.js
    fixed search bar

--- searchbar.js
+++ searchbar.js
@@ -7,22 +7,23 @@ let songs = []
 searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
     songs.forEach(song => {
-        const isVisible = song.songname.includes(value) || song.artist.includes(value)
+        const isVisible = 
+            song.songname.toLowerCase().includes(value) || 
+            song.artist.toLowerCase().includes(value)
         song.element.classList.toggle('hide', !isVisible)
     })
 })
 
 fetch('songdata.json')
-.then(res => res.json())
-.then(data => {
-    songs = data.map(song => {
-        const card = songCardTemplate.content.cloneNode(true).children[0]
-        const image = card.querySelector("data-img")
-        const header = card.querySelector("[data-header]")
-        const body = card.querySelector("[data-body]")
-        header.textContent = song.songname
-        body.textContent = song.artist
-        songCardContainer.append(card)
-        return { name: song.songname, artist: song.artist, element: card }
+    .then(res => res.json())
+    .then(data => {
+        songs = data.map(song => {
+            const card = songCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
+            header.textContent = song.songname
+            body.textContent = song.artist
+            songCardContainer.append(card)
+            return { name: song.songname, artist: song.artist, element: card }
     })
 })
