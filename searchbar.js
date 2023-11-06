0a8f8074c54b2a03f5cd8aab33611510d0d1db27
Zaid Seta <zaidmseta@gmail.com>
2023-11-06 09:28:14 -0500

    Fixed searchbar, it is now working flawlessly

--- searchbar.js
+++ searchbar.js
@@ -5,24 +5,25 @@ const searchInput = document.querySelector("[data-search]")
 let songs = []
 
 searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase()
-    songs.forEach(song => {
-        const isVisible = song.songname.includes(value) || song.artist.includes(value)
-        song.element.classList.toggle('hide', !isVisible)
-    })
+  const value = e.target.value.toLowerCase()
+  songs.forEach(song => {
+    const isVisible =
+      song.name.toLowerCase().includes(value) ||
+      song.artist.toLowerCase().includes(value)
+    song.element.classList.toggle("hide", !isVisible)
+  })
 })
 
-fetch('songdata.json')
-.then(res => res.json())
-.then(data => {
+fetch("songdata.json")
+  .then(res => res.json())
+  .then(data => {
     songs = data.map(song => {
-        const card = songCardTemplate.content.cloneNode(true).children[0]
-        const image = card.querySelector("data-img")
-        const header = card.querySelector("[data-header]")
-        const body = card.querySelector("[data-body]")
-        header.textContent = song.songname
-        body.textContent = song.artist
-        songCardContainer.append(card)
-        return { name: song.songname, artist: song.artist, element: card }
+      const card = songCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = song.songname
+      body.textContent = song.artist
+      songCardContainer.append(card)
+      return { name: song.songname, artist: song.artist, element: card }
     })
-})
+  })
