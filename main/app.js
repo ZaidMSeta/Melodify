c5fbaf6bfc021e4a882bfeadf711e7c576e72987
Dawan2 <dothman8927@hwdsb.on.ca>
2023-11-17 15:10:52 -0500

    missing content on last commit

--- main/app.js
+++ main/app.js
@@ -1,21 +1,71 @@
-const playlistTemplate = document.querySelector("[data-playlist-template]")
-const playlistContainer = document.querySelector("[data-playlist-container]")
+const categoryTemplate = document.querySelector("[data-category-template]");
+const playlistTemplate = document.querySelector("[data-playlist-template]");
+const playlistContainer = document.querySelector("[data-playlist-container]");
 
-let playlists = []
+let playlists = [];
 
 fetch("playlistdata.json")
   .then(res => res.json())
   .then(data => {
-    playlists = data.map(playlist => {
-      const card = playlistTemplate.content.cloneNode(true).children[0]
-      const playlist_header = card.querySelector("[data-playlist_header]")
-      const playlist_description = card.querySelector("[data-playlist_description]")
-      const playlist_image = card.querySelector("[data-playlist_image]") 
-      playlist_header.textContent = playlist.title
-      playlist_description.textContent = playlist.description
-      playlist_image.src = playlist.album_cover
-      playlistContainer.append(card)
-      return {title: playlist.title, description: playlist.description, image: playlist.album_cover}
-      //returned image above 
-    })
-  })
+    const categories = {};
+
+    // Group playlists by category
+    data.forEach(playlist => {
+      if (!categories[playlist.category]) {
+        categories[playlist.category] = [];
+      }
+      categories[playlist.category].push(playlist);
+    });
+
+    // Create category sections and populate playlists
+    for (const [category, playlistData] of Object.entries(categories)) {
+      const categorySection = categoryTemplate.content.cloneNode(true).children[0];
+      const categoryTitle = categorySection.querySelector("[data-category-title]");
+      const playlistCardsContainer = categorySection.querySelector("[data-playlist-cards]");
+
+      categoryTitle.textContent = category;
+
+      playlistData.forEach(playlist => {
+        const card = playlistTemplate.content.cloneNode(true).children[0];
+        const playlistHeader = card.querySelector("[data-playlist-header]");
+        const playlistDescription = card.querySelector("[data-playlist-description]");
+        const playlistImage = card.querySelector("[data-playlist-image]");
+
+        playlistHeader.textContent = playlist.title;
+        playlistDescription.textContent = playlist.description;
+        playlistImage.src = playlist.album_cover;
+
+        playlistCardsContainer.append(card);
+      });
+
+      playlistContainer.append(categorySection);
+
+      // Add the drag and slide functionality for each category
+      const categoryPlaylistCards = categorySection.querySelector("[data-playlist-cards]");
+      let isMouseDown = false;
+      let startX;
+      let scrollLeft;
+
+      categoryPlaylistCards.addEventListener('mousedown', (e) => {
+        isMouseDown = true;
+        startX = e.pageX - categoryPlaylistCards.offsetLeft;
+        scrollLeft = categoryPlaylistCards.scrollLeft;
+      });
+
+      categoryPlaylistCards.addEventListener('mouseleave', () => {
+        isMouseDown = false;
+      });
+
+      categoryPlaylistCards.addEventListener('mouseup', () => {
+        isMouseDown = false;
+      });
+
+      categoryPlaylistCards.addEventListener('mousemove', (e) => {
+        if (!isMouseDown) return;
+        e.preventDefault();
+        const x = e.pageX - categoryPlaylistCards.offsetLeft;
+        const walk = (x - startX) * 1.5; // Adjust the multiplier for smoother or faster scrolling
+        categoryPlaylistCards.scrollLeft = scrollLeft - walk;
+      });
+    }
+  });
