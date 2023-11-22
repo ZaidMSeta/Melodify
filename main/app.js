656881d625536afae62eadd4c9e04bf6ef1acffd
zaidmseta <zaidmseta@gmail.com>
2023-11-22 11:17:01 -0500

    added searchbar button

--- main/app.js
+++ main/app.js
@@ -81,4 +81,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
             }
         });
+        function goToSearchPage() {
+            // You can customize the search page URL as needed
+            window.location.href = 'searchbar.html';
+        }
+        document.getElementById('searchbutton').addEventListener('click', goToSearchPage);
 });
