60c5ae14e35d3c988a44d598f401cd86084fbdcf
Zaid Seta <zaidmseta@gmail.com>
2023-11-24 09:00:48 -0500

    login changes

--- signup_page/loginindex.php
+++ signup_page/loginindex.php
@@ -1,67 +1,62 @@
 <?php
     session_start();
+
+    include("pgp/config.php");
+
+    if(isset($_POST['submit'])){
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+
+        $result = mysqli_query($con, "SELECT * FROM users WHERE Email= '$email' AND Password = '$password' ") or die("Select Error");
+        $row = mysqli_fetch_assoc($result);
+
+        if(is_array($row) && !empty($row)){
+            $_SESSION['valid'] = $row['Email'];
+            $_SESSION['username'] = $row['Username'];
+            $_SESSION['age'] = $row['Age'];
+            $_SESSION['id'] = $row['Id'];
+            header("Location: hometest.php");
+            exit(); // Make sure to exit after a header redirect
+        } else {
+            echo "<div class='message'>
+                    <p>Wrong Username or Password!</p>
+                  </div> <br>";
+            echo "<a href='loginindex.php'><button class='button'>Go Back</button></a>";
+        }
+    }
 ?>
 <!DOCTYPE html>
-<html lang ="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content = "IE=edge">
-    <meta name="viewport" content ="width=device-width, inital-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/loginstyle.css">
     <title>Login</title>
 </head>
 <body>
-        <div class="container">
-            <div class="box form-box">
-                <?php 
-    
-                    include("pgp/config.php");
-                    if(isset($_POST['submit'])){
-                        $email = mysqli_real_escape_string($con,$_POST['email']);
-                        $password = mysqli_real_escape_string($con,$_POST['password']);
+    <div class="container">
+        <div class="box form-box">
+            <header>Login</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" required>
+                </div>
 
-                        $result = mysqli_query($con,"SELECT * FROM users WHERE Email= '$email' AND Password = '$password' ") or die("Select Error");
-                        $row = mysqli_fetch_assoc($result);
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" required>
+                </div>
 
-                        if(is_array($row) && !empty($row)){
-                            $_SESSION['valid'] = $row['Email'];
-                            $_SESSION['username'] = $row['Username'];
-                            $_SESSION['age'] = $row['Age'];
-                            $_SESSION['id'] = $row['Id'];
-                        }else{
-                            echo "<div class='message'>
-                                <p>Wrong Username or Password!</p>
-                                </div> <br>";
-                            echo "<a href='loginindex.php'><button class = 'button'>Go Back</button>";
-                        }
-                        if(isset($_SESSION['valid'])){
-                            header("Location: hometest.php");
-                        }
-                    }else{
-
-                ?>
-                <header>Login</header>
-                <form action="" method="post">
-                    <div class="field input">
-                        <label for="email">Email</label>
-                        <input type="text" name="email" id="email" required>
-                    </div>
-
-                    <div class="field input">
-                        <label for="password">Password</label>
-                        <input type="text" name="password" id="password" required>
-                    </div>
-
-                    <div class="field">
-                        
-                        <input type="submit" class = "button" name="submit" value="Login" required>
-                    </div>
-                    <div class="links">
-                        Don't have an account? <a href="register.php">Sign Up</a>
-                    </div>
-                </form>
-            </div>
-            <?php } ?>
+                <div class="field">
+                    <input type="submit" class="button" name="submit" value="Login" required>
+                </div>
+                <div class="links">
+                    Don't have an account? <a href="register.php">Sign Up</a>
+                </div>
+            </form>
         </div>
+    </div>
 </body>
 </html>
