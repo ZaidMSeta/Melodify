29fdd66d7914558981476784c90ec369e48a3cf2
Zaid Seta <zaidmseta@gmail.com>
2023-12-18 10:38:06 -0500

    added light mode to main page

--- main/app.js
+++ main/app.js
@@ -87,3 +87,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('searchbutton').addEventListener('click', goToSearchPage);
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Check the user's preference from previous visits (if available)
+    const storedLightMode = localStorage.getItem('lightMode');
+    if (storedLightMode === 'enabled') {
+        document.body.classList.add('light-mode');
+    }
+});
